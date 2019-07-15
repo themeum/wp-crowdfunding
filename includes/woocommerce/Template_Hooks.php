07084dbcc78f0b84db64f9c1a957ddf3b7d00cc7
MacBook Pro <hello@MacBooks-MacBook-Pro-2.local>
2019-07-15 18:06:43 +0600

    remove unnecessary file

--- includes/woocommerce/Template_Hooks.php
+++ includes/woocommerce/Template_Hooks.php
@@ -115,14 +115,6 @@ class Template_Hooks {
 		wpcf_function()->template('include/location');
 	}
 
-	public function wpneo_crowdfunding_campaign_single_left_div_start() {
-		wpcf_function()->template('include/single-left-div-start');
-	}
-
-	public function wpneo_crowdfunding_campaign_single_left_div_end() {
-		wpcf_function()->template('include/single-left-div-end');
-	}
-
 	public function campaign_single_tab() {
 		wpcf_function()->template('include/campaign-tab');
     }
@@ -137,14 +129,6 @@ class Template_Hooks {
 
 	public function single_fund_raised() {
 		wpcf_function()->template('include/fund-raised');
-	}
-
-	public function wpneo_crowdfunding_campaign_single_bakers_count_html() {
-		wpcf_function()->template('include/single-bakers-counter');
-	}
-
-	public function wpneo_crowdfunding_campaign_single_days_remaining() {
-		wpcf_function()->template('include/days-remaining');
 	}
 
 	public function single_fund_this_campaign_btn() {
