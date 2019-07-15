38d4b56715b477ad1ead96ad223542e1d0aad8fe
MacBook Pro <hello@MacBooks-MacBook-Pro-2.local>
2019-07-15 18:54:37 +0600

    remove uncessary files

--- wpcftemplate/woocommerce/basic/include/single-bakers-counter.php
+++
@@ -1,7 +0,0 @@
-<?php
-defined( 'ABSPATH' ) || exit;
-?>
-<div class="wpneo-single-sidebar">
-    <h3><?php echo wpcf_function()->get_total_backers(); ?></h3>
-    <p><?php _e('People backed this so far','wp-crowdfunding'); ?></p>
-</div>
--- wpcftemplate/woocommerce/basic/include/single-left-div-start.php
+++
@@ -1,4 +0,0 @@
-<?php
-defined( 'ABSPATH' ) || exit;
-?>
-<div class="wpneo-post-body float-left">
