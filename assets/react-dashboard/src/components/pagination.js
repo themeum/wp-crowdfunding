136379c8083a6e153f31eb6c0645be3992d34e17
MacBook Pro <hello@saju.local>
2019-08-20 18:35:58 +0600

    update pagination

--- assets/react-dashboard/src/components/pagination.js
+++ assets/react-dashboard/src/components/pagination.js
@@ -10,14 +10,12 @@ class Pagination extends Component {
     }
 
     componentWillMount() {
-        // set page if items array isn't empty
         if (this.props.items && this.props.items.length) {
             this.setPage(this.props.initialPage);
         }
     }
 
     componentDidUpdate(prevProps, prevState) {
-        // reset page if items array has changed
         if (this.props.items !== prevProps.items) {
             this.setPage(this.props.initialPage);
         }
@@ -30,36 +28,22 @@ class Pagination extends Component {
         if (currentPage < 1 || currentPage > pager.totalPages) {
             return;
         }
-        // get new pager object for specified page
         pager = this.getPager(items.length, currentPage);
-
-        // get new page of items from items array
         var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
-
-        // update state
         this.setState({ pager: pager });
-
-        // call change page function in parent component
         this.props.onChangePage(pageOfItems);
     }
 
     getPager(totalItems, currentPage) {
-        // default to first page
         currentPage = currentPage || 1;
-
-        // default page size is 10
         var pageSize = this.props.pageSize || 10;
-
-        // calculate total pages
         var totalPages = Math.ceil(totalItems / pageSize);
 
         var startPage, endPage;
         if (totalPages <= 10) {
-            // less than 10 total pages so show all
             startPage = 1;
             endPage = totalPages;
         } else {
-            // more than 10 total pages so calculate start and end pages
             if (currentPage <= 6) {
                 startPage = 1;
                 endPage = 10;
@@ -71,15 +55,10 @@ class Pagination extends Component {
                 endPage = currentPage + 4;
             }
         }
-
-        // calculate start and end item indexes
         var startIndex = (currentPage - 1) * pageSize;
         var endIndex = Math.min(startIndex + pageSize - 1, totalItems - 1);
-
-        // create an array of pages to ng-repeat in the pager control
         var pages = [...Array((endPage + 1) - startPage).keys()].map(i => startPage + i);
 
-        // return object with all pager properties required by the view
         return {
             totalItems: totalItems,
             currentPage: currentPage,
@@ -97,7 +76,6 @@ class Pagination extends Component {
         var pager = this.state.pager;
 
         if (!pager.pages || pager.pages.length <= 1) {
-            // don't display pager if there is only 1 page
             return null;
         }
 
