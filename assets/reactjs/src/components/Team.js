8e1b207a613dc2f49d0efb8dd9292983c762d2f3
Delowar Hosain <delowar6494@gmail.com>
2019-11-19 17:32:24 +0600

    Campaign Builder design updated

--- assets/reactjs/src/components/Team.js
+++ assets/reactjs/src/components/Team.js
@@ -1,11 +1,14 @@
 import { __ } from '@wordpress/i18n';
-import React, { Component } from 'react';
+import React, {Component, Fragment} from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import { isEmail, removeArrValue  } from '../Helper';
 import { reduxForm,  getFormValues, change as changeFieldValue } from 'redux-form';
 import { fetchUser } from '../actions';
 import PageControl from './Control';
+import Preview from "./preview/Preview";
+import PreviewBasic from "./preview/Basic";
+import PreviewEmpty from "./preview/Empty";
 
 const formName = "campaignForm";
 const sectionName = "team";
@@ -38,7 +41,7 @@ class Team extends Component {
 					emailInputMsg = __('Searching...', 'wp-crowdfunding');
 					this.props.fetchUser(value).then( response => {
 						this.setState({
-							member: response.user, 
+							member: response.user,
 							emailInputMsg: response.message
 						});
 					})
@@ -78,7 +81,7 @@ class Team extends Component {
 		this.props.changeFieldValue(formName, sectionName, team);
 		this.setState({member: memberFields, editMember: -1});
 	}
-	
+
 	_deleteMember(index) {
 		const { formValues: {team} } = this.props;
 		const values = removeArrValue(team, index);
@@ -86,7 +89,7 @@ class Team extends Component {
 	}
 
 	render() {
-		const { formValues: {team}, handleSubmit, current, prevStep, lastStep } = this.props;
+		const { formValues: {team}, handleSubmit, current, prevStep, lastStep, postId } = this.props;
 		const { member: {id, name, email, image, manage, edit}, emailInputMsg, editMember } = this.state;
 		return (
 			<div className="row">
@@ -99,69 +102,78 @@ class Team extends Component {
 								</div>
 								<div className='wpcf-accordion-details'>
 									<form>
-										<div className='wpcf-form-field'>
-											<div className='wpcf-field-title'>{ __('Email', 'wp-crowdfunding') }</div>
-											<div className="">
+										<div className='wpcf-form-group'>
+											<label className='wpcf-field-title'>{ __('Email', 'wp-crowdfunding') }</label>
+											<div>
 												<input type="email" value={email} onChange={(e) => this._onChange('email', e.target.value)}/>
-												<span>{emailInputMsg}</span>
+												<p className="wpcf-field-info">{emailInputMsg || "Enter Collaborator email address"}</p>
 											</div>
 										</div>
 
-										<div className='wpcf-form-field'>
-											<div className='wpcf-field-title'>{ __('Collaborator Name', 'wp-crowdfunding') }</div>
-											<div className="">
-												<input type="text" value={name} readOnly/>
-											</div>
-										</div>
+										{
+											id !== "" && <Fragment>
+												<div className='wpcf-form-group'>
+													<label className='wpcf-field-title'>{ __('Collaborator Name', 'wp-crowdfunding') }</label>
+													<div>
+														<input type="text" value={name} readOnly/>
+													</div>
+												</div>
 
-										<div className='wpcf-form-field'>
-											<div className='wpcf-field-title'>{ __('Collaborator Image', 'wp-crowdfunding') }</div>
-											<div className="">
-												<img className="profile-form-img" src={(image) ? image : ''} alt="Profile Image" />
-											</div>
-										</div>
+												<div className='wpcf-form-group'>
+													<div>
+														<img className="profile-form-img" src={(image) ? image : ''} alt="Profile Image" />
+													</div>
+												</div>
 
-										<div className='wpcf-form-field'>
-											<div className='wpcf-field-title'>{ __('If you Want to Show Contributor List', 'wp-crowdfunding') }</div>
-											<div className="">
-												<label className="checkbox-inline">
-													<input type="checkbox" checked={manage} onChange={(e) => this._onChange('manage', !manage)}/>
-													{ __('Give Permission to Manage Campaign', 'wp-crowdfunding') }
-												</label>
-											</div>
-										</div>
+												<div className='wpcf-form-group'>
+													<p className='wpcf-field-desc'>{ __('If you Want to Show Contributor List', 'wp-crowdfunding') }</p>
+													<label htmlFor="wpcf_manage_camp" className="wpcf-team-label">
+														<input id="wpcf_manage_camp" type="checkbox" checked={manage} onChange={(e) => this._onChange('manage', !manage)}/>
+														{ __('Give Permission to Manage Campaign', 'wp-crowdfunding') }
+													</label>
+												</div>
 
-										<div className='wpcf-form-field'>
-											<div className='wpcf-field-title'>{ __('If you Want to Show Contributor List', 'wp-crowdfunding') }</div>
-											<div className="">
-												<label className="checkbox-inline">
-													<input type="checkbox" checked={edit} onChange={(e) => this._onChange('edit', !edit)}/>
-													{ __('Give Permission to Edit Campaign', 'wp-crowdfunding') }
-												</label>
-											</div>
-										</div>
+												<div className='wpcf-form-group'>
+													<p className='wpcf-field-desc'>{ __('If you Want to Show Contributor List', 'wp-crowdfunding') }</p>
+													<label htmlFor="wpcf_edit_camp" className="wpcf-team-label">
+														<input id="wpcf_edit_camp" type="checkbox" checked={edit} onChange={(e) => this._onChange('edit', !edit)}/>
+														{ __('Give Permission to Edit Campaign', 'wp-crowdfunding') }
+													</label>
+												</div>
+
+											</Fragment>
+										}
 										{ editMember !== -1 ?
-											<button type="button" onClick={() => this._updateMember()}>{ __('Update Member', 'wp-crowdfunding') }</button> :
-											<button type="button" onClick={() => this._addMember()} disabled={!id}><span className="fa fa-plus"/>{ __('Add Member', 'wp-crowdfunding') }</button>
+											<button type="button" className="wpcf-btn wpcf-btn-round" onClick={() => this._updateMember()}>{ __('Update Member', 'wp-crowdfunding') }</button> :
+											<button type="button" className="wpcf-btn wpcf-btn-round" onClick={() => this._addMember()} disabled={!id}><span className="fa fa-plus wpcf-icon"/>{ __('Add Member', 'wp-crowdfunding') }</button>
 										}
 									</form>
 								</div>
 							</div>
 						</div>
-					
+
 						<div className="wpcf-team-members">
 							<h3>{ __('Team Members', 'wp-crowdfunding') }</h3>
-							{team && team.map((item, index) =>
-								<div key={index} className={`wpcf-reward-item ${(editMember == index) ? 'active':''}`}>
-									<p>{item.name}</p>
-									<p>{item.email}</p>
-									<img className="profile-form-img" src={item.image} alt="Profile Image" />
-									<span className="fa fa-trash" onClick={ () => this._deleteMember(index)}/>
-									<span className="fa fa-pencil" onClick={ () => this._editMember(index)}/>
-								</div>
-							)}
+							<div className="wpcf-team-member-items">
+								{team && team.map((item, index) =>
+									<div key={index} className={`wpcf-team-member ${(editMember == index) ? 'active':''}`}>
+										<div className="wpcf-team-avatar">
+											<img className="profile-form-img" src={item.image} alt="Profile Image" />
+										</div>
+										<div className="wpcf-team-content">
+											<h4>{item.name}</h4>
+											<h6>Campaigner</h6>
+											<p>{item.email}</p>
+										</div>
+										<div className="wpcf-team-overlay">
+											<button type="button" className="fa fa-trash" onClick={ () => this._deleteMember(index)}/>
+											<button type="button" className="fa fa-pencil" onClick={ () => this._editMember(index)}/>
+										</div>
+									</div>
+								)}
+							</div>
 						</div>
-						
+
 						<PageControl
                             current={current}
                             prevStep={prevStep}
@@ -169,10 +181,11 @@ class Team extends Component {
 					</form>
 				</div>
 				<div className='col-md-5'>
-                    <div className='wpcf-form-sidebar'>
-                        <div className="preview-title">{ __('Preview', 'wp-crowdfunding') }</div>
-						<p>{ __('Nothing to See here', 'wp-crowdfunding') }</p>
-                    </div>
+
+
+					<Preview postId={postId} title="Preview">
+						<PreviewEmpty/>
+					</Preview>
                 </div>
 			</div>
 		)
@@ -180,6 +193,7 @@ class Team extends Component {
 }
 
 const mapStateToProps = state => ({
+	postId: state.data.postId,
     formValues: getFormValues(formName)(state)
 });
 
--- assets/reactjs/src/components/preview/Preview.js
+++ assets/reactjs/src/components/preview/Preview.js
@@ -5,8 +5,12 @@ const Preview = (props) => {
     const {postId=0} = props
     return (
         <div className="wpcf-form-sidebar">
-            <div className="preview-title"><span className="fas fa-eye"></span> Preview</div>
-            {props.children && props.children}
+            <div className="preview-title">
+                <span className="fas fa-eye"></span> Preview
+            </div>
+            {
+                props.children && props.children
+            }
             <PreviewLink postId={postId}/>
         </div>
     )
