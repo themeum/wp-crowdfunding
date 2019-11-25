3c60f18080e88bc53c65b5dfe24f13dd3f4157ad
Delowar Hosain <delowar6494@gmail.com>
2019-11-25 17:14:02 +0600

    Campaign Builder design updated

--- assets/react-dashboard/src/components/Icon.js
+++ assets/react-dashboard/src/components/Icon.js
@@ -2,14 +2,14 @@ import React from 'react';
 import Icons from '../../../react-icon/Icons';
 
 const Icon = (props) => {
-    const {fill, stroke, name, className = '', tabIndex = '', onClick = ''} = props;
-    return (
-        <span className={"wpcf-svg-icon " + className} {...(tabIndex !== '' && {tabIndex})} {...(onClick !== '' && {onClick})}>
+  const {fill, stroke, name, className = '', tabIndex = '', onClick = ''} = props;
+  return (
+    <span className={"wpcf-svg-icon " + className} {...(tabIndex !== '' && {tabIndex})} {...(onClick !== '' && {onClick})}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox={Icons[name].viewBox}>
                 <path fillRule="nonzero" {...(fill && {fill})}  {...(name && {d: Icons[name].d})} {...(stroke && { stroke })}/>
             </svg>
         </span>
-    )
+  )
 }
 
 export default Icon;
