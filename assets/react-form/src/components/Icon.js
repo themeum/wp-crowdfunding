5b60ecef8e12a6c205fa25195c9c6c4ce28ced14
Delowar Hosain <delowar6494@gmail.com>
2019-11-25 13:45:39 +0600

    Campaign Builder design updated

--- assets/react-form/src/components/Icon.js
+++ assets/react-form/src/components/Icon.js
@@ -2,9 +2,9 @@ import React from 'react';
 import Icons from '../Icons';
 
 const Icon = (props) => {
-    const {fill, stroke, name, className = ''} = props;
+    const {fill, stroke, name, className = '', tabIndex = '', onClick = ''} = props;
     return (
-        <span className={"wpcf-svg-icon " + className}>
+        <span className={"wpcf-svg-icon " + className} {...(tabIndex !== '' && {tabIndex})} {...(onClick !== '' && {onClick})}>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox={Icons[name].viewBox}>
                 <path fillRule="nonzero" {...(fill && {fill})}  {...(name && {d: Icons[name].d})} {...(stroke && { stroke })}/>
             </svg>
--- assets/react-form/src/components/Reward.js
+++ assets/react-form/src/components/Reward.js
@@ -7,6 +7,7 @@ import RenderRewardFields from './fields/Reward';
 import PreviewReward from './preview/Reward';
 import PageControl from './Control';
 import Preview from "./preview/Preview";
+import Icon from './Icon'
 
 const formName = "campaignForm";
 const sectionName = "rewards";
@@ -91,7 +92,8 @@ class Reward extends Component {
 														key={index}
 														className={`wpcf-rewards-option ${(selectedType == index) ? 'active':''}`}
 														onClick={() => this.setState({selectedType: index})}>
-														<span className="fas fa-shopping-cart"></span>
+														<Icon name={item.icon}/>
+														{/*<span className="fas fa-shopping-cart"></span>*/}
 														<p>{item.title}</p>
 													</div>
 												)}
@@ -130,8 +132,8 @@ class Reward extends Component {
 												{item.title && <h6>{item.title}</h6>}
 												{item.description && <p>{item.description}</p>}
 												<div className="wpcf-reward-item-overlay">
-													<span className="fa fa-trash" onClick={() => this._deleteReward(index)}/>
-													<span className="fa fa-pencil" onClick={() => this._editReward(index)}/>
+													<Icon tabIndex="0" name="delete" onClick={() => this._deleteReward(index)}/>
+													<Icon tabIndex="0" name="pen" onClick={() => this._editReward(index)}/>
 												</div>
 											</div>
 										)
@@ -142,11 +144,7 @@ class Reward extends Component {
 								</div>
 							</div>
 						</div>
-
-						<PageControl
-                            current={current}
-                            prevStep={prevStep}
-                            lastStep={lastStep}/>
+						<PageControl current={current} prevStep={prevStep} lastStep={lastStep} />
 					</form>
 				</div>
 				<div className='col-md-5'>
