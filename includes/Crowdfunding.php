1e62109950746d2ffb23dbc06f7b787c80007c34
sksaju <sksaaju@gmail.com>
2021-03-17 17:02:54 +0600

    update base file

--- includes/Crowdfunding.php
+++ includes/Crowdfunding.php
@@ -16,6 +16,7 @@ final class Crowdfunding {
 
 	function __construct() {
 		$this->includes_core();
+		$this->include_apis();
 		$this->include_shortcode();
 		$this->include_addons();
 		$this->initial_activation();
@@ -32,6 +33,13 @@ final class Crowdfunding {
 		require_once WPCF_DIR_PATH.'includes/Gutenberg.php';
 		new settings\Admin_Menu();
 		new \WPCF\Gutenberg();
+	}
+
+	// Include APIs
+	public function include_apis() {
+		if( class_exists( 'WooCommerce' ) ){
+			include_once WPCF_DIR_PATH.'includes/APIs/Dashboard.php';
+		}
 	}
 
 	//Checking Vendor
