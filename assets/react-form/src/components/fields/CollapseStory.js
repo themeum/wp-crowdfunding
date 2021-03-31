f892ff2a5fb32b94d8286bb5232713305c9366b9
Maliha Noushin Raida <malihanoushin@iut-dhaka.edu>
2021-03-31 16:30:47 +0600

    collapsing story added

---
+++ assets/react-form/src/components/fields/CollapseStory.js
@@ -0,0 +1,62 @@
+import React from 'react';
+
+const CollapseVideo = ({ data }) => {
+	if (data.value && data.value.length > 0) {
+		const videoName = data.value[0].name;
+		return (
+			<div className='wpcf-story-item-image'>
+				<i className='fas fa-video'></i>
+				{videoName}
+			</div>
+		);
+	}
+	return (
+		<div className='wpcf-story-item-image '>
+			<i className='fas fa-video'></i>
+		</div>
+	);
+};
+
+const CollapseImage = ({ data }) => {
+	if (data.value && data.value.length > 0) {
+		const imageName = data.value[0].name;
+		return (
+			<div className='wpcf-story-item-image'>
+				<i className='fas fa-image'></i>
+				{imageName}
+			</div>
+		);
+	}
+	return (
+		<div className='wpcf-story-item-image '>
+			<i className='fas fa-image'></i>
+		</div>
+	);
+};
+
+const extractTextFromHTML = (value) => {
+	var div = document.createElement('div');
+	div.innerHTML = value;
+	var text = div.textContent;
+	console.log(text.length);
+	if (text.length > 30) {
+		var str = text.slice(0, 20) + '...';
+	}
+	return str || text;
+};
+
+const RenderCollapseStory = ({ data }) => {
+	switch (data.type) {
+		case 'video':
+			return <CollapseVideo data={data} />;
+		case 'image':
+			return <CollapseImage data={data} />;
+		case 'embeded':
+		case 'text':
+			return <div>{extractTextFromHTML(data.value)}</div>;
+		default:
+			return null;
+	}
+};
+
+export default RenderCollapseStory;
--- assets/react-form/src/components/fields/Story.js
+++ assets/react-form/src/components/fields/Story.js
@@ -3,67 +3,91 @@ import Editor from './Editor';
 
 //FIELD TYPES
 const Image = (props) => {
-    const { name, data, upload } = props;
-    let imageVal = [];
-    if(data.value && data.value.length > 0) {
-        const imageSrc = data.value[0].src;
-        imageVal = data.value;
-        return (
-            <div className="wpcf-story-item-image" onClick={() => upload(name, 'image', imageVal)}><img src={imageSrc}/></div>
-        );
-    }
-    return (
-        <div className="wpcf-story-item-image wpcf-empty-image" onClick={() => upload(name, 'image', imageVal)}>
-            <span className="fas fa-image"></span>
-        </div>
-    )
-}
+	const { name, data, upload } = props;
+	let imageVal = [];
+	if (data.value && data.value.length > 0) {
+		const imageSrc = data.value[0].src;
+		imageVal = data.value;
+		return (
+			<div
+				className='wpcf-story-item-image'
+				onClick={() => upload(name, 'image', imageVal)}
+			>
+				<img src={imageSrc} />
+			</div>
+		);
+	}
+	return (
+		<div
+			className='wpcf-story-item-image wpcf-empty-image'
+			onClick={() => upload(name, 'image', imageVal)}
+		>
+			<span className='fas fa-image'></span>
+		</div>
+	);
+};
 
 const Video = (props) => {
-    const { name, data, upload } = props;
-    return (
-        <div className="story-item-image" onClick={() => upload(name, 'video', [])}>
-            { data.value && data.value.length > 0 ?
-                <video controls><source src={data.value[0].src} type={data.value[0].mime}/>Your browser does not support the video tag</video>
-            :   <img src={`${WPCF.assets}images/story-default-image.png`}/> }
-        </div>
-    );
-}
+	const { name, data, upload } = props;
+	return (
+		<div
+			className='story-item-image'
+			onClick={() => upload(name, 'video', [])}
+		>
+			{data.value && data.value.length > 0 ? (
+				<video controls>
+					<source src={data.value[0].src} type={data.value[0].mime} />
+					Your browser does not support the video tag
+				</video>
+			) : (
+				<div className='wpcf-story-item-image wpcf-empty-image'>
+					<span className='fas fa-video'></span>
+				</div>
+			)}
+		</div>
+	);
+};
 
 const EmbededFile = (props) => {
-    const { name, data, edit } = props;
-    return (
-        <div className="story-item-embeded_file">
-            { data.value ?
-                <div dangerouslySetInnerHTML={{__html: data.value}} />
-            :   <textarea value={data.value} onChange={(e) => edit(name, e.target.value)}/> }
-        </div>
-    );
-}
+	const { name, data, edit } = props;
+	return (
+		<div className='story-item-embeded_file'>
+			{data.value ? (
+				<div dangerouslySetInnerHTML={{ __html: data.value }} />
+			) : (
+				<textarea
+					value={data.value}
+					onChange={(e) => edit(name, e.target.value)}
+				/>
+			)}
+		</div>
+	);
+};
 
 const TextEditor = (props) => {
-    const { name, data, edit } = props;
-    return (
-        <Editor
-            value={data.value}
-            className="wpcf-story-texteditor"
-            onChange={(value) => edit(name, value)}/>
-    );
-}
+	const { name, data, edit } = props;
+	return (
+		<Editor
+			value={data.value}
+			className='wpcf-story-texteditor'
+			onChange={(value) => edit(name, value)}
+		/>
+	);
+};
 
 const RenderStoryItem = (props) => {
-    switch (props.data.type) {
-        case 'image':
-            return <Image {...props}/>
-        case 'video':
-            return <Video {...props}/>
-        case 'embeded':
-            return <EmbededFile {...props}/>
-        case 'text':
-            return <TextEditor {...props}/>
-        default:
-            return null;
-    }
-}
+	switch (props.data.type) {
+		case 'image':
+			return <Image {...props} />;
+		case 'video':
+			return <Video {...props} />;
+		case 'embeded':
+			return <EmbededFile {...props} />;
+		case 'text':
+			return <TextEditor {...props} />;
+		default:
+			return null;
+	}
+};
 
 export default RenderStoryItem;
