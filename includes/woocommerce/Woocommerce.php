d22b0cac6ef3c7a2de6eada44b2ea3ddf4177ca3
Md Rashed Hossain <wprashed@icloud.com>
2024-10-23 12:52:03 +0600

    Update Woocommerce.php

--- includes/woocommerce/Woocommerce.php
+++ includes/woocommerce/Woocommerce.php
@@ -142,6 +142,27 @@ class Woocommerce {
 		return $fields;
 	}
 
+	// Remove billing address fields from WooCommerce checkout page
+	add_filter( 'woocommerce_checkout_fields', 'remove_billing_address_fields' );
+
+	function remove_billing_address_fields( $fields ) {
+		// Remove all billing fields
+		unset( $fields['billing']['billing_first_name']);
+		unset( $fields['billing']['billing_last_name']);
+		unset( $fields['billing']['billing_company']);
+		unset( $fields['billing']['billing_address_1']);
+		unset( $fields['billing']['billing_address_2']);
+		unset( $fields['billing']['billing_city']);
+		unset( $fields['billing']['billing_postcode']);
+		unset( $fields['billing']['billing_country']);
+		unset( $fields['billing']['billing_state']);
+		unset( $fields['billing']['billing_phone']);
+		unset( $fields['billing']['billing_email']);
+
+		return $fields;
+	}
+
+
 	/**
 	 * Change checkout button text callback
 	 *
